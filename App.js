f9e1756262a6dfe69de9896304fa1703acdda2eb
Jason Boerner <jboerner56@gmail.com>
2019-07-09 13:21:23 -0400

    chat imput

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import io from 'socket.io-client';
 
-class App extends React.Component() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: '',
+      
+    };
+  }
   // socket.io connection in componentDidMount so it connects on first page load
   componentDidMount() {
-    const socket = io('http://127.0.0.1:3000');
+    this.socket = io('http://127.0.0.1:3000');
   };
-
+  submitChatMessage() {
+    this.socket.emit('chat message', this.state.chatMessage)
+    this.setState({ chatMessage: '' });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput 
+        // inline styling, can also do separatly. better to do separately if doing much more styling
+        style={
+          {
+            height: 40,
+            borderWidth: 2,
+            marginTop: 10,
+          }
+        }
+        value={this.state.chatMessage}
+        autoCorrect={false}
+        // function that will submit the message to the backend
+        onSubmit={() => this.submitChatMessage}
+        onChangeText={chatMessage => {
+                          // es6 syntax. actually means chatMessage: chatMessage
+                          // since the names are the same the second can be ommitted
+          this.setState({ chatMessage });
+        }}/>
       </View>
     );
   }
@@ -21,8 +48,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
-export default App;
